7fab4dfff04f4e62b6ad131ac6e4962821d35d08
Celest21 <zc.chetty@gmail.com>
2023-04-20 10:11:52 -0700

    amendment

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,22 +1,11 @@
-#!/bin/bash
-#include<stdio.h>
-
+#include <stdio.h>
 /**
-
-* main - Entry point
-
+*main - entry point
 *
-
-* Return: Always 0 (Success)
-
+*Return: always 0 (success)
 */
-
 int main(void)
-
 {
-
-        printf("with proper grammar, but the outcome is a piece of art, ");
-
-        return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
