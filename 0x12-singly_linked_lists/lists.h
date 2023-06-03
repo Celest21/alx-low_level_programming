4cf9ace1db356afd8ad5e2f8735fa954b226609d
Celest21 <zc.chetty@gmail.com>
2023-06-02 18:02:33 -0700

    amendments

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,11 +3,11 @@
 
 /**
   * struct list_s - this is a singly linked list
-  * @str - used to store the string value and
+  * @str: used to store the string value and
   * is a pointer to a character
-  * @len - the unsigned interger and stores the 
+  * @len: the unsigned interger and stores the
   * lenght of the string
-  * @next - it is a pointer, each element points to the next
+  * @next: it is a pointer, each element points to the next
   *
   * Description - a singly linked structure
   */
@@ -20,6 +20,9 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 int _putchar(char c);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
-
